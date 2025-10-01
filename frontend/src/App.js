fb40d4deb8d6b9138acc180042f181740daf208e
DaminiSuranshe <daminisuranshe29@gmail.com>
2025-10-01 23:37:02 +0530

    new file:   frontend/.gitignore 	new file:   frontend/README.md 	new file:   frontend/package-lock.json 	new file:   frontend/package.json 	new file:   frontend/public/favicon.ico 	new file:   frontend/public/index.html 	new file:   frontend/public/logo192.png 	new file:   frontend/public/logo512.png 	new file:   frontend/public/manifest.json 	new file:   frontend/public/robots.txt 	new file:   frontend/src/App.css 	new file:   frontend/src/App.js 	new file:   frontend/src/index.css 	new file:   frontend/src/index.js

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚨 Cyber Intelligence Platform</h1>
+      <p>Hello Cyber World! 👩‍💻</p>
+    </div>
+  );
+}
+
+export default App;
